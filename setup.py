a9bb7ee11ebaab578f8ee29e4e6a014c72a18fa2
Henri De Plaen <henri.deplaen@gmail.com>
2024-02-11 23:30:56 +0100

    CI/CL (compilation supported per version)

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup
-from torch.utils import cpp_extension
-from sys import platform
+from sys import platform, version_info
 
 if __name__ == "__main__":
-    if platform == "win32":
-        extra_compile_args = ['/O2', '/std:c++17']
+    if platform == "win32" or platform: # windows (not supported yet)
+        # extra_compile_args = ['/O2', '/std:c++17']
+        setup()
+    elif platform == 'darwin' and version_info.minor > 11 : # mac greater than python 3.11
+        setup()
     else:
+        from torch.utils import cpp_extension
         extra_compile_args = ['-O2', '-std=c++17']
-
-    setup(
-        ext_modules=[cpp_extension.CppExtension(name='uotod.compiled',
-                                                sources=['src/cpp/sinkhorn.cpp'],
-                                                include_dirs=['src/cpp'],
-                                                extra_compile_args=extra_compile_args)]
-    )
+        setup(
+            ext_modules=[cpp_extension.CppExtension(name='uotod.compiled',
+                                                    sources=['src/cpp/sinkhorn.cpp'],
+                                                    include_dirs=['src/cpp'],
+                                                    extra_compile_args=extra_compile_args)]
+        )
