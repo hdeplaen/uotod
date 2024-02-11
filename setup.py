866dbbf826337091d2b949887abbdb58c7f89d8f
Henri De Plaen <henri.deplaen@gmail.com>
2024-02-11 22:51:35 +0100

    CI/CL (compiler flags)

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 from torch.utils import cpp_extension
+from sys import platform
 
 if __name__ == "__main__":
+    if platform == "win32":
+        extra_compile_args = ['/O2', '/std:c++17']
+    else:
+        extra_compile_args = ['-O2', '-std=c++17']
+
     setup(
         ext_modules=[cpp_extension.CppExtension(name='uotod.compiled',
                                                 sources=['src/cpp/sinkhorn.cpp'],
                                                 include_dirs=['src/cpp'],
-                                                extra_compile_args=['-O2', '-std=c++17',        # unix-like
-                                                                    '/O2', '/std:c++17'])],     # windows
+                                                extra_compile_args=extra_compile_args)]
     )
